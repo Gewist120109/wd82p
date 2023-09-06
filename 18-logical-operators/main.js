18a7fe4c0b44c05c15787b86485d3332aeb4eb8d
unknown <armina.gewist@gmail.com>
2023-09-06 20:48:21 +0800

    add rock-paper-scissor

--- 18-logical-operators/main.js
+++ 18-logical-operators/main.js
@@ -1,15 +1,1 @@
-// user input
-
-let userInput = parseInt(prompt("Enter a Number: "));
-
-for (let i = 1; i <= userInput; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+let carBrand = ["isuzu", "ford", "toyota", "mitsubishi"];
